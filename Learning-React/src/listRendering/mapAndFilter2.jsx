e713009ca58a419d3da0767a923402aeb2127c0e
zaneDevsThat <zane.webdev@gmail.com>
2024-02-27 02:06:22 +0800

    mapAndFilter2.jsx added

---
+++ Learning-React/src/listRendering/mapAndFilter2.jsx
@@ -0,0 +1,201 @@
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+
+{/*My answer: */}
+export default function List() {
+  const chemist = people.filter(person => person.profession === 'chemist')
+  const nonChemist = people.filter(person => person.profession !== 'chemist')
+  const listItems = chemist.map(person =>
+      <li key={person.id}>
+        <img
+          src={getImageUrl(person)}
+          alt={person.name}
+        />
+        <p>
+          <b>{person.name}:</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+        </p>
+      </li>
+  );
+  const others = nonChemist.map(person => 
+      <li key={person.id}>
+        <img
+          src={getImageUrl(person)}
+          alt={person.name}
+        />
+        <p>
+          <b>{person.name}:</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+        </p>
+      </li>
+  );
+      
+
+  
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <h2>Chemists</h2>
+      <ul>{listItems}</ul>
+      <h2>Everyone Else</h2>
+      <ul>{others}</ul>
+    </article>
+  );
+}
+
+
+{/*react.dev's solutions */}
+{/*Solution 1 
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+
+export default function List() {
+  const chemists = people.filter(person =>
+    person.profession === 'chemist'
+  );
+  const everyoneElse = people.filter(person =>
+    person.profession !== 'chemist'
+  );
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <h2>Chemists</h2>
+      <ul>
+        {chemists.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+      <h2>Everyone Else</h2>
+      <ul>
+        {everyoneElse.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+    </article>
+  );
+}
+*/}
+{/*Solution 2
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+
+function ListSection({ title, people }) {
+  return (
+    <>
+      <h2>{title}</h2>
+      <ul>
+        {people.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+    </>
+  );
+}
+
+export default function List() {
+  const chemists = people.filter(person =>
+    person.profession === 'chemist'
+  );
+  const everyoneElse = people.filter(person =>
+    person.profession !== 'chemist'
+  );
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <ListSection
+        title="Chemists"
+        people={chemists}
+      />
+      <ListSection
+        title="Everyone Else"
+        people={everyoneElse}
+      />
+    </article>
+  );
+}
+*/}
+{/*Solution 3
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+
+let chemists = [];
+let everyoneElse = [];
+people.forEach(person => {
+  if (person.profession === 'chemist') {
+    chemists.push(person);
+  } else {
+    everyoneElse.push(person);
+  }
+});
+
+function ListSection({ title, people }) {
+  return (
+    <>
+      <h2>{title}</h2>
+      <ul>
+        {people.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+    </>
+  );
+}
+
+export default function List() {
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <ListSection
+        title="Chemists"
+        people={chemists}
+      />
+      <ListSection
+        title="Everyone Else"
+        people={everyoneElse}
+      />
+    </article>
+  );
+}
+*/}
