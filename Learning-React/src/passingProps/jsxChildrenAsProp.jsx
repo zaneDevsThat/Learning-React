4a50cea646b7097aaab30a46b06585fb07f059db
zaneDevsThat <zane.webdev@gmail.com>
2024-03-01 05:27:35 +0800

    added another solution for jsxChildrenAsProp.jsx

--- Learning-React/src/passingProps/jsxChildrenAsProp.jsx
+++ Learning-React/src/passingProps/jsxChildrenAsProp.jsx
@@ -28,3 +28,37 @@ export default function Profile() {
     </div>
   );
 }
+
+
+{/*You can also do it this way 
+function Card({children, title}) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </div>
+  )
+}
+export default function Profile() {
+  return (
+    <div>
+        <Card title="Photo">
+          <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={100}
+          height={100}
+          />
+        </Card>
+        <Card title="About">
+          <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+        </Card>
+    </div>
+  );
+}
+
+
+*/}
